95526bde59371b5bffa85b394c4162027c130143
Mirosław Nowak <mnowak0995@gmail.com>
2023-08-08 14:03:14 +0200

    commit

--- work_shop2/src/main/java/pl/coderslab/TaskManager.java
+++ work_shop2/src/main/java/pl/coderslab/TaskManager.java
@@ -1,5 +1,15 @@
 package pl.coderslab;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class TaskManager {
-    
+    public static void main(String[] args) {
+        try {
+            Connection conn = DbUtil.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
